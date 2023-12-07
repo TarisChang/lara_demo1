a981829d1726a441dda054301afb7de4b86ce567
Taris <taris.chang@hlwtech.com.tw>
2023-12-07 12:54:17 +0800

    edit bug

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 @section('title', '關於我們')
 @section('main')
     <!--正文begin-->
-    <div class="about_bg" style="background: url(/assets/images/world.png) center top no-repeat;">
+    <div class="about_bg" style="background: url(/assets/images/world.png) center top no-repeat;background-size:cover;">
         <div class="about_con anim anim-1" >
             {!! $global['about'] ?? '' !!}
         </div>
--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -4,7 +4,7 @@
     <style>
     </style>
     <!--正文begin-->
-    <div class="about_bg2" style="background: url(/assets/images/history.png) center top no-repeat;">
+    <div class="about_bg2" style="background: url(/assets/images/history.png) center top no-repeat;background-size:cover;">
         <div class="about_con anim anim-1">
             {!! $global['history'] ?? '' !!}
         </div>
