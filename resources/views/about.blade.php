55c34a0c4cfd7c983f4a8d0e630e3ff41446fd26
Taris <taris.chang@hlwtech.com.tw>
2024-01-22 17:14:05 +0800

    statis

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,7 @@
 ﻿@extends('layouts.app')
 @section('title', 'About Us')
 @section('main')
-    <div class="about_bg" style="background: url(/assets/images/world.png) center top no-repeat;background-size:cover;">
+    <div class="about_bg" style="background: url(/assets/images/world2.png) center top no-repeat;background-size:cover;">
         <div class="about_con anim anim-1" >
             {!! $global['about'] ?? '' !!}
         </div>
